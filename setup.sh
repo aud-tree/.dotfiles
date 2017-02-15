8293206777fd2b3dffa464ca647c3fced02771e8
audreyschwarz <acbschwarz@gmail.com>
2017-02-15 17:23:37 -0600

    Install vim-plug on setup instead of committing copy

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@ for dotfile in .gitconfig .zshrc .tmux.conf .vimrc .vim; do
   [ -e ~/$dotfile ] && mv ~/$dotfile ~/$dotfile.old
   ln -s ~/.dotfiles/$dotfile ~/$dotfile
 done
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
